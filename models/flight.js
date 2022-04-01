27ff09c6c139c33301488db3393a1de219c27d9e
rosacf-ga <rc.floress@yahoo.com>
2022-03-31 18:34:51 -0700

    Add flight model

---
+++ models/flight.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema; 
+
+const flightSchema = new Schema ({
+    airline: String, 
+    airport: String, 
+    flightNo: Number,
+    departs: Date
+})
+
+module.exports = mongoose.model('Flight', flightSchema);

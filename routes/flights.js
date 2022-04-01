1abe9c38660f28b3cd5672fb8859a07950079701
rosacf-ga <rc.floress@yahoo.com>
2022-03-31 18:35:54 -0700

    Create initial routes

--- routes/flights.js
+++ routes/flights.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const flightsController = require('../controllers/flights')
+
+//will allow us to read all flights
+router.get('/', flightsController.index);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//create new flight
+router.post('/', flightsController.create);
+
+//return form to add a new post
+router.get('/new', flightsController.new)
 
 module.exports = router;

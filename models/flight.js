89c691cd85fcd813a809fbdf322adedab89b31a6
rosacf-ga <rc.floress@yahoo.com>
2022-04-01 19:20:07 -0700

    Add destinations schema

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema; 
 
+const destinationSchema = new Schema ({
+	airport: {
+		type: String, 
+		enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+	}, 
+	arrival: {
+		type: Date, 
+		default: function(){
+			let today = new Date(); 
+			let nextYear = today.setFullYear(today.getFullYear()+1);
+			return nextYear
+		}
+	}
+})
+
 const flightSchema = new Schema({
     airline: {
 			type: String, 
@@ -24,7 +39,8 @@ const flightSchema = new Schema({
 				let nextYear = today.setFullYear(today.getFullYear()+1);
 				return nextYear
 			}
-		}
+		}, 
+	destinations: [destinationSchema]
 })
 
 module.exports = mongoose.model('Flight', flightSchema);

0d36f25bf9a320b584399fe5ebdf4923b3772973
rosacf-ga <rc.floress@yahoo.com>
2022-04-03 11:29:34 -0700

    Redirect localhost:3000 to flights index page upon page load

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+//this redirects to main flights index (home page)
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/flights');
 });
 
 module.exports = router;

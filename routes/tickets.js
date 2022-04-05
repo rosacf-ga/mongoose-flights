84ed946a14e7b4bb7ff0158ac2339e09aa981ebf
rosacf-ga <rc.floress@yahoo.com>
2022-04-04 17:07:25 -0700

    Remove commented out code

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ticketsCtrl = require('../controllers/tickets');
 
-// router.get('/performers/new', performersCtrl.new);
-// router.post('/performers', performersCtrl.create);
-// router.post('/movies/:id/performers', performersCtrl.addToCast)
 
 router.get('/flights/:id/tickets/new', ticketsCtrl.new);
 router.post('/flights/:id/tickets', ticketsCtrl.create)
